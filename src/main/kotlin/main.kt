89c7b03078fd3205ae896fd437919db665c09a46
DionatanLeao <dionatan.ti@outlook.com>
2022-10-31 15:23:42 -0300

    O problema de tipos diferentes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,21 @@
 import domain.Funcionario
 
 fun main() {
-    println("---------------------------------")
-    println("----- Bem vindo ao Bytebank -----")
-    println("---------------------------------")
+    println("------------------------------------------------------------------")
+    println("------------------- Bem vindo ao Bytebank ------------------------")
+    println("------------------------------------------------------------------")
 
     val funcionarioUm = Funcionario(
         "funcionario um",
         "111.111.111-11",
-        1000.0
+        1000.0,
+        0
     )
 
-    println("nome ${funcionarioUm.nome}")
-    println("cpf ${funcionarioUm.cpf}")
-    println("salario ${funcionarioUm.salario}")
-    println("bonificação ${funcionarioUm.bonificacao()}")
+    println("nome: ${funcionarioUm.nome}")
+    println("cpf: ${funcionarioUm.cpf}")
+    println("salario: ${funcionarioUm.salario}")
+    println("bonificação: ${funcionarioUm.bonificacao()}")
 }
 
 
