7d050191acf9b2ae109f900e4bdb7ca414ccb4f1
DionatanLeao <dionatan.ti@outlook.com>
2022-10-31 15:12:40 -0300

    Criando uma nova classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,20 @@
+import domain.Funcionario
+
 fun main() {
     println("---------------------------------")
     println("----- Bem vindo ao Bytebank -----")
     println("---------------------------------")
-    comportamentosConta()
+
+    val funcionarioUm = Funcionario(
+        "funcionario um",
+        "111.111.111-11",
+        1000.0
+    )
+
+    println("nome ${funcionarioUm.nome}")
+    println("cpf ${funcionarioUm.cpf}")
+    println("salario ${funcionarioUm.salario}")
+    println("bonificação ${funcionarioUm.bonificacao()}")
 }
 
 
