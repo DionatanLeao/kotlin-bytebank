8b772df71136e4b3332352b421a28c9440852d6a
DionatanLeao <dionatan.ti@outlook.com>
2022-11-09 14:55:35 -0300

    Utilizando variáveis globais

--- src/br.com.alura.bytebank/kotlin/main.kt
+++ src/br.com.alura.bytebank/kotlin/main.kt
@@ -1,9 +1,20 @@
+import modelo.Cliente
+import modelo.ContaCorrente
+import modelo.ContaPoupanca
+import modelo.totalContas
 import teste.autenticacao
 import teste.contasDiferentes
 
 fun main() {
     autenticacao()
     contasDiferentes()
+
+    val cliente = Cliente(nome = "Cliente", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = cliente, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = cliente, numero = 1001)
+
+    println("Total de contas: $totalContas")
+
 }
 
 
