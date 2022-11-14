033a07e61ee2e6ea4f93708172703d2d7cffdef0
DionatanLeao <dionatan.ti@outlook.com>
2022-11-14 09:39:17 -0300

    Lidando com exceptions com o try expression

--- src/br.com.alura.bytebank/kotlin/main.kt
+++ src/br.com.alura.bytebank/kotlin/main.kt
@@ -6,15 +6,21 @@ fun main() {
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
-    funcao2()
+
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        println("ClassCastException foi pegada")
+    }
+
     println("fim funcao1")
 }
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+    for (i in 1..5) {
         println(i)
         val endereco = Any()
         endereco as Endereco
