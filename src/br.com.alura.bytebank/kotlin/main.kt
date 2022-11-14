b9cab3b990f8c72fc3f84764f5623f67bd00eb93
DionatanLeao <dionatan.ti@outlook.com>
2022-11-14 15:20:30 -0300

    Utilizando o safe call

--- src/br.com.alura.bytebank/kotlin/main.kt
+++ src/br.com.alura.bytebank/kotlin/main.kt
@@ -1,9 +1,12 @@
 import modelo.Endereco
 
 fun main() {
-    var enderecoNulo: Endereco? = null
-    val enderecoNaoNulo: Endereco =  enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua A")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo = null
+    enderecoNulo?.let { endereco: Endereco ->
+        println(endereco.logradouro.length)
+    }
 }
 
 
