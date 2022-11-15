4218c8d9163e983c4227398e5f58b423c4c441b5
DionatanLeao <dionatan.ti@outlook.com>
2022-11-15 12:24:28 -0300

    Iterando por um array

--- src/br.com.alura.bytebank/kotlin/main.kt
+++ src/br.com.alura.bytebank/kotlin/main.kt
@@ -1,7 +1,22 @@
 fun main() {
-    val idades = IntArray(4)
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
+    val idades = intArrayOf(25, 19, 33, 20, 55, 40)
+
+    var maiorIdade = 0
+
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+
+    println(maiorIdade)
+
+    var menorIdade = Int.MAX_VALUE
+
+    idades.forEach { idade ->
+        if (idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+    println(menorIdade)
 }
