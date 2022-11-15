231e11b1af082fc99c26aac73d0698f8cacaf78a
DionatanLeao <dionatan.ti@outlook.com>
2022-11-15 12:41:49 -0300

    Alterando um elemento

---
+++ src/br.com.alura.bytebank/kotlin/teste/iterandoArray.kt
@@ -0,0 +1,24 @@
+package teste
+
+fun iterandoArray() {
+    val idades = intArrayOf(25, 19, 33, 20, 55, 40)
+
+    var maiorIdade = 0
+
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+
+    println(maiorIdade)
+
+    var menorIdade = Int.MAX_VALUE
+
+    idades.forEach { idade ->
+        if (idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+    println(menorIdade)
+}
