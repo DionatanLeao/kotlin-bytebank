e70511f259fb879467ac8ca6184e940f35fde491
DionatanLeao <dionatan.ti@outlook.com>
2022-12-02 10:06:01 -0300

    Comportamentos do Set

--- src/br.com.alura.bytebank/kotlin/collections/list-set.kt
+++ src/br.com.alura.bytebank/kotlin/collections/list-set.kt
@@ -2,6 +2,10 @@ package collections
 
 fun main() {
 //    list()
+//    set()
+}
+
+private fun set() {
     val assistiramCursoAndroid = mutableSetOf("Nome1", "Nome2", "Nome3", "Nome4")
     val assistiramCursoKotlin = mutableSetOf("Nome1", "Nome5", "Nome4")
     val assistiramAmbos = mutableSetOf<String>()
@@ -9,7 +13,9 @@ fun main() {
     assistiramAmbos.addAll(assistiramCursoKotlin)
     assistiramAmbos.add("Nome6")
     println(assistiramAmbos)
-
+    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
+    println(assistiramCursoAndroid.subtract(assistiramCursoKotlin))
+    println(assistiramCursoAndroid.intersect(assistiramCursoKotlin))
 }
 
 private fun list() {
