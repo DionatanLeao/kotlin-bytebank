7125e4498a8f9edbac01c264ca22ecc3180af1bd
DionatanLeao <dionatan.ti@outlook.com>
2022-12-02 14:31:29 -0300

    Utilizando o agrupamento

--- src/br.com.alura.bytebank/kotlin/collections/associacao.kt
+++ src/br.com.alura.bytebank/kotlin/collections/associacao.kt
@@ -5,7 +5,8 @@ fun main() {
         Pedido(1, 20.00),
         Pedido(2, 60.00),
         Pedido(3, 30.00),
-        Pedido(4, 70.00)
+        Pedido(4, 70.00),
+        Pedido(5, 40.00)
     )
 
     println(pedidos)
@@ -25,6 +26,36 @@ fun main() {
 
     println(pedidosFreteGratis)
     println(pedidosFreteGratis[Pedido(numero = 2, valor = 60.0)])
+
+//    val mapa = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//    println(mapa)
+    val pedidosFreteGratisAgrupados = pedidos.groupBy { pedido ->
+        pedido.valor > 50.0
+    }
+
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[true])
+
+    val nomes = listOf(
+        "Nome1",
+        "Nome2",
+        "Nome3",
+        "Nome4",
+        "Nome5",
+        "Nome6",
+        "Nome7",
+        "Nome8"
+    )
+
+    val agenda = nomes.groupBy { nome ->
+        nome.first()
+    }
+
+    println(agenda)
+    println(agenda['A'])
+
 }
 
 data class Pedido(
