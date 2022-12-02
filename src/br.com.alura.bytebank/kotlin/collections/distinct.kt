5f213da69fb1059a3fceb4190b608fe256844917
DionatanLeao <dionatan.ti@outlook.com>
2022-12-02 09:53:08 -0300

    Utilizando o Set

--- src/br.com.alura.bytebank/kotlin/collections/distinct.kt
+++ src/br.com.alura.bytebank/kotlin/collections/distinct.kt
@@ -1,8 +1,20 @@
 package collections
 
 fun main() {
+//    list()
+    val assistiramCursoAndroid = mutableSetOf("Nome1", "Nome2", "Nome3", "Nome4")
+    val assistiramCursoKotlin = mutableSetOf("Nome1", "Nome5", "Nome4")
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Nome6")
+    println(assistiramAmbos)
+
+}
+
+private fun list() {
     val assistiramCursoAndroid = listOf("Nome1", "Nome2", "Nome3", "Nome4")
-    val assistiramCursoKotlin = listOf("Nome1", "Nome5","Nome4")
+    val assistiramCursoKotlin = listOf("Nome1", "Nome5", "Nome4")
     val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
 //    assistiramAmbos.addAll(assistiramCursoAndroid)
 //    assistiramAmbos.addAll(assistiramCursoKotlin)
