bc7067fe732ea490a00c093ba9c6f1627ae7578b
DionatanLeao <dionatan.ti@outlook.com>
2022-12-02 11:27:07 -0300

    Comportamentos de escrita do Map

--- src/br.com.alura.bytebank/kotlin/collections/list-set.kt
+++ src/br.com.alura.bytebank/kotlin/collections/list-set.kt
@@ -1,19 +1,37 @@
 package collections
 
 fun main() {
-//    list()
-//    set()
-    val pedidos = mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    list()
+    set()
+    map()
+}
+
+private fun map() {
+    val pedidos = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
     println(pedidos)
     val pedido = pedidos[3]
     pedido?.let {
         println("Pedido $it")
     }
-
     pedidos.forEach {
         println("número do pedido: ${it.key}")
         println("valor do pedido: ${it.value}")
     }
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5, 80.0)
+    println(pedidos)
+    pedidos.put(1, 100.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6, 200.0)
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+    pedidos.remove(6)
+    println(pedidos)
 }
 
 private fun set() {
