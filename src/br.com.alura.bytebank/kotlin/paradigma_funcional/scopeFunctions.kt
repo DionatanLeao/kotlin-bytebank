04bb69cea5566b29b9c56efbc5c7583bad414a00
DionatanLeao <dionatan.ti@outlook.com>
2022-12-06 12:15:39 -0300

    Utilização das scope functions

--- src/br.com.alura.bytebank/kotlin/paradigma_funcional/scopeFunctions.kt
+++ src/br.com.alura.bytebank/kotlin/paradigma_funcional/scopeFunctions.kt
@@ -3,13 +3,34 @@ package paradigma_funcional
 import modelo.Endereco
 
 fun main() {
+    val endereco = Endereco(logradouro = "rua a", numero = 3184)
 
-    Endereco(logradouro = "rua a", numero = 3185)
+    Endereco(logradouro = "rua b", numero = 3185)
+        .also { println("Criando endereço") }
+        .apply {
+            "$logradouro, $numero".toUpperCase()
+        }.let { enderecoMaiusculo ->
+            println(enderecoMaiusculo)
+        }
+
+    Endereco(logradouro = "rua c", numero = 3186)
         .run {
             "$logradouro, $numero".toUpperCase()
         }.let { enderecoMaiusculo ->
             println(enderecoMaiusculo)
         }
+
+    run {
+        println("execução do run sem extensão")
+    }
+
+    with(endereco) {
+        "$logradouro, $numero".toUpperCase()
+    }.let { enderecoMaiusculo ->
+        println(enderecoMaiusculo)
+    }
+
+    println()
 
     val enderecoComplemento = listOf(
         Endereco(complemento = "casa"),
